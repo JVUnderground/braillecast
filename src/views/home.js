c638e9525f88e63138ea1a325079ff8fd1b5cfcb
João Carneiro <joao.carneiro@gmail.com>
2019-02-10 23:09:17 -0200

    bootstrap installed

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Input , FormGroup, Label} from 'reactstrap';
 
 export default function Home(props) {
     return (
         <Container>
             <h1>Home View</h1>
+            <FormGroup>
+                <Label htmlFor="convert-video">Converte!</Label>
+                <Input id="convert-video"/>
+            </FormGroup>
         </Container>
     );
 }
